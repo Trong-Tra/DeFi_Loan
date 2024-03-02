9ee65e94453cf8d37094b55c4cc6ee826fefdaf3
Minh Trong <traminhtrong2604@gmail.com>
2024-03-03 03:05:14 +0700

    build: restricted user input when create a loan

--- x/loan/types/message_request_loan.go
+++ x/loan/types/message_request_loan.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"strconv"
+
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -21,7 +23,32 @@ func NewMsgRequestLoan(creator string, amount string, fee string, collateral str
 func (msg *MsgRequestLoan) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "Invalid creator address (%s)", err)
+	}
+	amount, _ := sdk.ParseCoinNormalized(msg.Amount)
+	if !amount.IsValid() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Amount is not a valid Coins object")
+	}
+	if amount.Empty() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Amount is empty")
+	}
+	fee, _ := sdk.ParseCoinNormalized(msg.Fee)
+	if !fee.IsValid() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Fee is not a valid Coins object")
+	}
+	deadline, err := strconv.ParseInt(msg.Deadline, 10, 64)
+	if err != nil {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Deadline is not an integer")
+	}
+	if deadline <= 0 {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Deadline need to be a positive integer")
+	}
+	collateral, _ := sdk.ParseCoinNormalized(msg.Collateral)
+	if !collateral.IsValid() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "Collateral is not a valid Coiins object")
+	}
+	if collateral.Empty() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "collateral is empty")
 	}
 	return nil
 }
